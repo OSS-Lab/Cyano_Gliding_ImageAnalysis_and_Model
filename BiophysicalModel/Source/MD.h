41c349aee19a52848f8cae597fbf5d2d3bb8a442
elocatelli <53816951+elocatelli@users.noreply.github.com>
2024-02-14 13:07:08 +0100

    Add files via upload
    Added source files for biophysical model - v0

---
+++ BiophysicalModel/Source/MD.h
@@ -0,0 +1,256 @@
+
+/*_idx == monomer id */
+
+
+void harmonic_interaction(int _id)
+{
+    double d;
+    int j;
+    // use nx for pfx if _id = 0  (False) -- predictor
+    // use px for fx  if _id = 1 (True) -- corrector
+    
+    for(j = 0; j < Sys.NPart-1; j++){
+	if(_id) d = myparticle[j+1].px-myparticle[j].px-DHpot.x0[j];
+	else d = myparticle[j+1].nx-myparticle[j].nx-DHpot.x0[j];
+	myparticle[j].ds = d;
+	if( d < 0){
+            if(_id){ myparticle[j].fx += DHpot.kcomp[j]*d; myparticle[j+1].fx += -DHpot.kcomp[j+1]*d;}
+	    else{ myparticle[j].pfx += DHpot.kcomp[j]*d; myparticle[j+1].pfx += -DHpot.kcomp[j+1]*d;}
+	}else{
+	    if(_id){ myparticle[j].fx += DHpot.kstr[j]*d; myparticle[j+1].fx += -DHpot.kstr[j+1]*d;}
+	    else{ myparticle[j].pfx += DHpot.kstr[j]*d; myparticle[j+1].pfx += -DHpot.kstr[j+1]*d;}
+	}	
+    }
+
+    myparticle[Sys.NPart-1].ds = myparticle[Sys.NPart-2].ds;
+
+}
+
+double confining_field(int _id){
+     double temp;
+     
+     if(myparticle[_id].theta > Sys.theta_th){ 
+         temp = myparticle[_id].theta-Sys.theta_th;
+	 return -2*(temp*temp*temp);
+     }else if (myparticle[_id].theta < -Sys.theta_th){
+         temp = myparticle[_id].theta+Sys.theta_th;
+         return -2*(temp*temp*temp);
+     }else {
+         return 0.;
+     }  
+}
+
+double harmonic_single(int _id)
+{
+    double out = 0;
+    double d;
+
+    if(_id != 0){ 
+	d = myparticle[_id].nx-myparticle[_id-1].nx-DHpot.x0[_id];
+        if ( d < 0){
+	    out += -(Sys.efield+Sys.Kom)*d; 
+	}else{
+	    out += -(Sys.efield+Sys.Kom)*d; 
+	}
+    }
+
+    if(_id != Sys.NPart-1){
+        d = myparticle[_id+1].nx-myparticle[_id].nx-DHpot.x0[_id];
+        if ( d < 0){
+	    out += (Sys.efield+Sys.Kom)*d; 
+        }else{
+	    out += (Sys.efield+Sys.Kom)*d; 	    
+        }
+    }
+
+    return out;
+}
+
+double harmonic_energy(int _id)
+{
+    double out = 0;
+    double d;
+
+    if (_id != 0) { 
+        d = myparticle[_id].nx-myparticle[_id-1].nx-DHpot.x0[_id];
+	if( d < 0 ) out += 0.5*DHpot.kcomp[_id]*d*d;
+	else out += 0.5*DHpot.kstr[_id]*d*d;
+    }
+    if (_id != Sys.NPart-1){
+        d = myparticle[_id+1].nx-myparticle[_id].nx-DHpot.x0[_id];
+        if( d < 0 ) out += 0.5*DHpot.kcomp[_id]*d*d;
+        else out += 0.5*DHpot.kstr[_id]*d*d;
+    }
+
+    return out;
+}
+
+void active_push(int _id)
+{
+   int j;
+   
+   for(j = 0; j < Sys.NPart; j++){
+       if(_id) myparticle[j].fx += myparticle[j].fa;
+       else myparticle[j].pfx += myparticle[j].fa;
+   }  
+}
+
+void force_nullify(int _id)
+{
+    long int j;
+    if(_id) for(j = 0; j < Sys.NPart; j++) myparticle[j].fx = 0.;
+    else for(j = 0; j < Sys.NPart; j++) myparticle[j].pfx = 0.;
+}
+
+void forces_predictor()
+{
+    force_nullify(0);
+    harmonic_interaction(0);
+    active_push(0);
+
+}
+
+void forces_corrector()
+{
+    force_nullify(1);
+    harmonic_interaction(1);
+    active_push(1);
+}
+
+
+/* 
+ * Different models:
+ * Model 0: all monomers are active, 
+ * Model 1: only first and last monomers are active
+ * Model 2: only first or last monomer is active
+ */
+
+void update_stop_field()
+{
+   int j;
+
+   for(j=0; j < Sys.NPart; j++){
+	if( (myparticle[j].nx > 0 && myparticle[j].nx < Sys.box_x) ){ myparticle[j].stop_field = 0;}
+	if( myparticle[j].nx < 0 ){ 
+		myparticle[j].stop_field = Sys.efield; Sys.active_end = 0;  
+	}
+	if( myparticle[j].nx > Sys.box_x ){ 
+		myparticle[j].stop_field = -Sys.efield; Sys.active_end = Sys.NPart-1;
+	}
+   }
+
+}
+
+void active_status()
+{
+
+    int j;
+    int count = 0;
+    double DeltaE, rnd;
+ 
+    for(j = 0; j < Sys.NPart; j++){
+        if(Sys._model == 1 && (j != 0 && j != Sys.NPart-1)) continue;
+        if(Sys._model == 2 && j != Sys.active_end && (myparticle[j].is_active) == 0) continue;
+	myparticle[j].theta += confining_field(j)*Sys.Dt + myparticle[j].stop_field*Sys.Dt + harmonic_single(j)*Sys.Dt + sqrt(2*Sys.Tt*Sys.Dt/1.)*gaussrand(&Sys.seed);
+
+        myparticle[j].is_active = threshold(tanh(myparticle[j].theta)); 
+	myparticle[j].is_active_d = tanh(myparticle[j].theta);
+    }	
+   
+    if(Sys._model == 0){   
+            for(j = 0; j < Sys.NPart; j++) myparticle[j].fa = myparticle[j].is_active_d*Sys.myfa;
+    }else if(Sys._model == 1){ /* ONLY ENDS ARE ACTIVE */
+	    myparticle[0].fa = myparticle[0].is_active_d*Sys.myfa; myparticle[Sys.NPart-1].fa = myparticle[Sys.NPart-1].is_active_d*Sys.myfa;
+    }else if(Sys._model == 2){
+            for(j = 0; j < Sys.NPart; j+=Sys.NPart-1) myparticle[j].fa = myparticle[j].is_active_d*Sys.myfa;
+    }else{
+            printf("error: entry not valid\n");
+            exit(1);
+    }
+    
+}
+
+/*************************************************************************/
+
+void overdamped_predictor()
+{
+    long int j;
+    double zz;
+
+    for(j = 0; j < Sys.NPart; j++) {
+        zz = Sys.zeta[1]; 
+	myparticle[j].px = myparticle[j].nx + (Sys.Dt/zz)*myparticle[j].pfx + sqrt(2*Sys.T*Sys.Dt/zz)*gaussrand(&Sys.seed);  
+    }
+  
+}
+
+void overdamped_corrector()
+{
+    double eta2;
+    double zz;
+    long int j;
+
+    for(j = 0; j < Sys.NPart; j++){ 
+	zz = Sys.zeta[1];
+        myparticle[j].nx += 0.5*(Sys.Dt/zz)*(myparticle[j].fx+myparticle[j].pfx) + sqrt(2*Sys.T*Sys.Dt/zz)*gaussrand(&Sys.seed);   
+    }
+}
+
+void print_status(){
+   
+    int j;
+
+    printf("PRINT STATUS:\n");
+    for(j = 0; j < Sys.NPart; j++){
+        printf("%d %.5f %.5f %.5f %d %.3f\n", j, myparticle[j].nx, myparticle[j].fx, myparticle[j].ds, myparticle[j].stop_field, myparticle[j].theta/PI);
+    }
+    printf("----------------------------------\n");
+
+
+}
+
+void integrate_position()
+{
+    update_stop_field();
+    active_status();  
+
+    forces_predictor();
+    overdamped_predictor();
+
+    forces_corrector();
+    overdamped_corrector();
+
+    if(Sys.step % Sys.v_lag == 0) compute_velocity();
+}
+
+void do_BD()
+{
+    char dumpname[100];
+    char dumpname_all[100];
+    char restartname[100];
+
+    sprintf(dumpname,"trajectory.lammpstrj");
+    FILE *f = fopen(dumpname, "w"); fclose(f);
+
+    sprintf(dumpname_all,"trajectory_all.lammpstrj");
+    f = fopen(dumpname_all, "w"); fclose(f);
+
+    sprintf(restartname,"restartpoint.dat");
+
+    double msdratio=1.; 
+
+    for(Sys.step = Sys.restart_step; Sys.step < Sys.restart_step+Sys.NSteps; Sys.step++){ 
+       
+	if(Sys.step % 10000 == 0)  WriteConf(restartname);
+	
+	if(Sys.step % Sys.NPrint == 0) Write_trajectory(dumpname);
+	if(Sys.step % (Sys.NPrint) == 0) Write_trajectory_all(dumpname_all);
+
+	integrate_position();
+	if(Sys.step % 1000 == 0) print_status();
+
+
+    }
+
+}
+
---
+++ BiophysicalModel/Source/init.h
@@ -0,0 +1,133 @@
+void read_input_file()
+{
+    FILE *_myfile = fopen("param.dat", "r");
+    
+    char line[100];
+    int i=0;
+    char *token;
+    char *_key=NULL;
+    char *_value = NULL;
+    
+    Sys.restart_step = 0;
+
+    while(fgets(line, sizeof(line), _myfile) != NULL){
+
+      token = strtok(line, " ");
+      i = 0;
+      while (token != NULL) {
+          if (i == 0) _key = token;
+	  if (i == 2) _value = token;
+          token = strtok(NULL, " ");
+          i++;
+      }     
+   
+      if(strcmp("N_particles",_key) == 0){ Sys.NPart = atoi(_value); continue; }
+      if(strcmp("box_x",_key) == 0){ Sys.box_x = atof(_value); continue; } 
+      if(strcmp("temperature",_key) == 0){ Sys.T = atof(_value); continue; }
+      if(strcmp("theta_temperature",_key) == 0){ Sys.Tt = atof(_value); continue; }
+      if(strcmp("friction",_key) == 0){ Sys.zeta_null = atof(_value); continue; } 
+      if(strcmp("timestep",_key) == 0){ Sys.Dt = atof(_value); continue; }
+      
+      if(strcmp("model_type",_key) == 0){ Sys._model = atoi(_value); continue; }
+          
+      if(strcmp("N_steps",_key) == 0){ Sys.NSteps = atoi(_value); continue; }
+      if(strcmp("ratio_samplings",_key) == 0){ Sys.NPrint = atoi(_value); continue; }
+      if(strcmp("ratio_restartfile",_key) == 0){ Sys.Nrestart = atoi(_value); continue; } 
+      if(strcmp("step_restart",_key) == 0){ Sys.restart_step = atoi(_value); continue; }
+      if(strcmp("restart_key",_key) == 0){ Sys.restart = atoi(_value); continue; }      
+     
+      if(strcmp("external_field",_key) == 0){ Sys.efield = atof(_value); continue; }
+      if(strcmp("delta_Komega",_key) == 0){ Sys.Kom = atof(_value); continue; }
+      if(strcmp("theta_threshold",_key) == 0){ Sys.theta_th = atof(_value); continue; } 
+      if(strcmp("active_force",_key) == 0){ Sys.myfa = atof(_value); continue; } 
+
+      if(strcmp("myseed",_key) == 0){ Sys.seed = atoi(_value); continue; } 
+      
+      if( Sys.mypot == 0){
+
+          DHpot.N = Sys.NPart;
+	  if(strcmp("r0",_key) == 0){ DHpot.X0 = atof(_value); continue; }
+	  if(strcmp("k_compression",_key) == 0){ DHpot.Kcomp = atof(_value); continue; }
+	  if(strcmp("k_stretching",_key) == 0){ DHpot.Kstr = atof(_value); continue; }
+
+          DHpot.x0 = (double*) malloc (DHpot.N*sizeof(double));
+          DHpot.kstr = (double*) malloc (DHpot.N*sizeof(double));
+          DHpot.kcomp = (double*) malloc (DHpot.N*sizeof(double));
+        }
+
+    }
+    
+    fclose(_myfile);
+
+    sprintf(Sys.restart_file,"restart_file.dat");
+
+}
+
+void init_system()
+{
+    long int j;
+   
+    if(Sys.NPart*DHpot.X0 > Sys.box_x){ printf("Error: box too small! enlarging ... \n"); Sys.box_x = 1.2*Sys.NPart*DHpot.X0+2; }
+ 
+    if(Sys.restart == 0) 
+        for(j = 0; j < Sys.NPart; j++){
+            myparticle[j].nx = myparticle[j].px = j+1;
+	    myparticle[j].ox = myparticle[j].nx;
+	    myparticle[j].x = P_Img(myparticle[j].nx,Sys.box_x); 
+	    myparticle[j].theta = 0.;
+	    myparticle[j].stop_field = 0; 
+	}
+
+    else{
+        ReadConf();
+    }
+ 
+    /*ZETA*/
+    Sys.zeta = (double *) malloc (3*sizeof(double));
+    Sys.zeta[0] = Sys.zeta_null; Sys.zeta[1] = 1*Sys.zeta_null; Sys.zeta[2] = Sys.zeta_null;
+
+    // for model=1
+    Sys.v_lag = 100;
+    Sys.active_end = 0;
+
+    /***SET HARMONIC PARAMETERS***/
+    for(j = 0; j < Sys.NPart; j++)
+    {
+      DHpot.kcomp[j] = DHpot.Kcomp;
+      DHpot.kstr[j] = DHpot.Kstr;
+      DHpot.x0[j] = DHpot.X0;	          
+    }
+
+    
+    for(j = 0; j < Sys.NPart; j++) { 
+	myparticle[j].theta = 0;   
+	if(Sys._model == 0) myparticle[j].is_active = 0; 
+	else if(Sys._model == 1){
+	    if(j == 0 || j == Sys.NPart-1){ 
+		myparticle[j].is_active = 0;
+		myparticle[j].is_active_d = 0;
+	    	myparticle[j].stop_field = 0.; 
+	    }
+	    else myparticle[j].is_active = 0;
+	}
+	else if(Sys._model == 2){
+            myparticle[j].is_active = 0;
+            if(j == Sys.active_end){ 
+	        myparticle[j].is_active = 0;
+		myparticle[j].is_active_d = 0;
+		myparticle[j].stop_field = 0.; 
+	    }
+        }
+        	
+	myparticle[j].fa = myparticle[j].is_active_d*Sys.myfa;
+    } 
+    
+    for(j = 0; j < Sys.NPart; j++) myparticle[j].fx = myparticle[j].pfx = 0.;
+
+    Sys.D0 = Sys.T/(Sys.zeta_null); 
+    
+}
+
+
+
+
